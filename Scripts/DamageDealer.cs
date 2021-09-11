35061cda7f5c25134a56afe428e155570b01fae7
JENNNBI <87286877+JENNNBI@users.noreply.github.com>
2021-09-11 12:02:28 +0300

    Add files via upload

---
+++ Scripts/DamageDealer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    int damage = 100;
+
+    public float GetDamage()
+    {
+        return damage;
+    }
+
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+}
